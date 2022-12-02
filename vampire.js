2f819dac276e0e1fe41d1da7e09b7ee7315f8bb8
Thien Trieu <thien.trieu@telus.com>
2022-12-02 13:11:06 -0800

    vampr.js

--- vampire.js
+++ vampire.js
@@ -10,22 +10,37 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
+    this.offspring.push(vampire);
+    vampire.creator = this
 
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
 
+    return numberOfVampires
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    let result = false
+    
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal){
+      return true
+    }
+    return result
   }
 
   /** Stretch **/
@@ -36,9 +51,30 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    // let closest = this
+    
+    // if (this.numberOfVampiresFromOriginal > vampire.numberOfVampiresFromOriginal){
+    //   closest = vampire
+    // }
+    // return closest
 
   }
 }
+
+const francis = new Vampire('Francis', 2019)
+const computer = new Vampire('Computer', 2022)
+const emily = new Vampire('Emily', 2021)
+const woowoo = new Vampire('WooWoo', 2022)
+
+francis.addOffspring(emily)
+francis.addOffspring(computer)
+emily.addOffspring(woowoo)
+
+
+console.log(francis)
+
+console.log(emily.closestCommonAncestor(computer))
+
 
 module.exports = Vampire;
 
